a3a01d52d500914dfff1f4fe6c3e1d1b2259e9a1
NikolaVeselinovic <nikolaveselinovic388@gmail.com>
2017-04-05 21:28:54 +0200

    Napravljen JUnitTestCase TwitterPorukaTest, nisu napisani testovi

---
+++ Twitter/test/com/twitter/poruke/TwitterPorukaTest.java
@@ -0,0 +1,34 @@
+package com.twitter.poruke;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TwitterPorukaTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testSetKorisnik() {
+		
+	}
+
+	@Test
+	public void testSetPoruka() {
+		
+	}
+
+	@Test
+	public void testToString() {
+		
+	}
+
+}
