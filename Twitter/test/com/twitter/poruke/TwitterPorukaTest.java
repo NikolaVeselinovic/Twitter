348921557d65c835bc478cb84af6ed78a26063db
NikolaVeselinovic <nikolaveselinovic388@gmail.com>
2017-04-07 18:12:11 +0200

    Napisani JUnit testova za set metode i toString metodu; u metodi setKorisnik testiranjem pronadjena greska u uslovu i ispravljena ! kod poruka.isEmpty(); u getPoruka povratna vrednost bila string "poruka", prepravljeno na vrednost atributa poruka; u setPoruka ispravljeni uslovi.

--- Twitter/test/com/twitter/poruke/TwitterPorukaTest.java
+++ Twitter/test/com/twitter/poruke/TwitterPorukaTest.java
@@ -1,5 +1,7 @@
 package com.twitter.poruke;
 
+
+
 import static org.junit.Assert.*;
 
 import org.junit.After;
@@ -7,7 +9,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TwitterPorukaTest {
-
+	TwitterPoruka p=new TwitterPoruka();;
 	@Before
 	public void setUp() throws Exception {
 	}
@@ -16,19 +18,49 @@ public class TwitterPorukaTest {
 	public void tearDown() throws Exception {
 	}
 
+	@Test (expected = java.lang.RuntimeException.class) 
+	public void testSetKorisnikNull() {
+		p=new TwitterPoruka();
+		p.setKorisnik(null);
+		
+	}
+	@Test (expected = java.lang.RuntimeException.class) 
+	public void testSetKorisnikEmpty() {
+		p=new TwitterPoruka();
+		p.setKorisnik("");
+		
+	}
 	@Test
 	public void testSetKorisnik() {
 		
+		p.setKorisnik("Nikola");
+		assertEquals("Nikola",p.getKorisnik());
+		
 	}
 
+	@Test (expected = java.lang.RuntimeException.class)
+	public void testSetPorukaNull() {
+		p.setPoruka(null);
+	}
+	@Test (expected = java.lang.RuntimeException.class)
+	public void testSetPorukaEmpty() {
+		p.setPoruka("");
+	}
+	@Test (expected = java.lang.RuntimeException.class)
+	public void testSetPorukaLong() {
+		p.setPoruka(null);
+	}
 	@Test
 	public void testSetPoruka() {
-		
+		p.setPoruka("Zdravo");
+		assertEquals("Zdravo",p.getPoruka());
 	}
 
 	@Test
 	public void testToString() {
-		
+		p.setKorisnik("nikola");
+		p.setPoruka("zdravo");
+		assertEquals("KORISNIK:nikola PORUKA:zdravo",p.toString());
 	}
 
 }
